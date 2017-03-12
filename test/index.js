f13de9bb136325ec5b3968c37a82d69a6879f1e6
Lev Himmelfarb <lev@boylesoftware.com>
2017-03-12 18:49:36 -0400

    Tests are work in progress.

--- test/index.js
+++ test/index.js
@@ -4,10 +4,41 @@ const expect = require('chai').expect;
 
 const records = require('../index.js');
 
+
+const TEST_DEFS = {
+	'Record1': {
+		properties: {
+			'id': {
+				valueType: 'number',
+				role: 'id'
+			}
+		}
+	}
+};
+
+
 describe('x2node-records', function() {
+
 	describe('.buildLibrary()', function() {
+
 		it('should return RecordTypesLibrary', function() {
 			expect(records.buildLibrary({})).to.be.ok;
+		});
+
+	});
+
+	describe('RecordTypesLibrary', function() {
+
+		const recordTypes = records.buildLibrary(TEST_DEFS);
+		it('should have Record1', function() {
+			expect(recordTypes.hasRecordType('Record1')).to.be.true;
+		});
+
+		const recordTypeDesc = recordTypes.getRecordTypeDesc('Record1');
+		describe('RecordTypeDescriptor', function() {
+			it('should have id property', function() {
+				expect(recordTypeDesc.idPropertyName).to.equal('id');
+			});
 		});
 	});
 });
